ea008567c25598a4e5e452c8b70f18b8e3f61fd7
Val <vp.kiss@gmail.com>
2021-09-15 14:35:59 +0200

    initial commit

--- test.py
+++ test.py
@@ -4,4 +4,4 @@ BASE_URL = "http://127.0.0.1:5000/"
 
 payload = {"first_name": "Val", "last_name": "Kiss", "age": 26, "email": "test@test.com"}
 response = requests.patch(BASE_URL + "students/1", params=payload)
-print(response.text)
+print(response.text) 
